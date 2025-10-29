2057608d5cc4e16549f52747fcb56ffd5308b6d2
Gerald <gderhemi22@epoka.edu.al>
2025-10-29 21:14:07 +0100

    Fix path case sensitivity for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Title from './Components/Title/Title'
 import About from './Components/About/About'
 import Campus from './Components/Campus/Campus'
 import Testimonials from './Components/Testimonials/Testimonials'
-import Contact from './Components/contact/Contact'
+import Contact from './Components/Contact/Contact'
 import Footer from './Components/Footer/Footer'
 
 
